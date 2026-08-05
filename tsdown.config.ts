c7381b4ffc257920b058d55a065e1546bb6fc50f
lihbr <lihbr@users.noreply.github.com>
2026-08-05 11:21:58 +0900

    build: output

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "tsdown"
 
 export default defineConfig({
-	entry: ["./src/index.ts", "./src/node/index.ts"],
+	entry: {
+		index: "./src/index.ts",
+		node: "./src/node/index.ts",
+	},
 	format: ["esm", "cjs"],
 	platform: "neutral",
 	unbundle: true,
